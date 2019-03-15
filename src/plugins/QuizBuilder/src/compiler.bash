da95d2d58bc891a183b96c8581563e196a4bac45
Tim St.Clair <tim.stclair@gmail.com>
2019-03-15 13:49:12 +1100

    updated build process

--- src/plugins/QuizBuilder/src/compiler.bash
+++ src/plugins/QuizBuilder/src/compiler.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 TS=$(date +"%Y%m%d%H%M%S")
 
 echo "Compiling templates"
@@ -7,17 +9,17 @@ echo "Minifying App"
 rm ../app.min*.css
 rm ../app.min*.js
 uglifycss app.css --output ../app.min.$TS.css
-uglifyjs --keep-fnames localforage.min.js handlebars.min.js filedrop.js jscolor.min.js --output app.deps.min.js
+# uglifyjs --keep-fnames localforage.min.js handlebars.min.js filedrop.js jscolor.min.js --output app.deps.min.js
 uglifyjs templates.js app.js --output ../app.min.$TS.js
 
 echo "Updating references"
 cp edit.html ..
 #rm -rf templates.js
 if [ "$(uname)" == "Darwin" ]; then
-	sed -E -i '' "s/app.min.(.*)css/app.min.$TS.css/g" ../edit.html
-	sed -E -i '' "s/app.min.(.*)js/app.min.$TS.js/g" ../edit.html
+	sed -E -i '' "s/app.css/app.min.$TS.css/g" ../edit.html
+	sed -E -i '' "s/app.js/app.min.$TS.js/g" ../edit.html
 else
 #elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	sed -E --in-place='' "s/app.min.(.*)css/app.min.$TS.css/g" ../edit.html
-	sed -E --in-place='' "s/app.min.(.*)js/app.min.$TS.js/g" ../edit.html
+	sed -E --in-place='' "s/app.css/app.min.$TS.css/g" ../edit.html
+	sed -E --in-place='' "s/app.js/app.min.$TS.js/g" ../edit.html
 fi
