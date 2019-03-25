9954728d3f918725eb2b8f0b148286c6a970605a
Tim St.Clair <tim.stclair@gmail.com>
2019-03-26 09:17:53 +1100

    changed script names to actually work

--- public/app/plugins/QuizBuilder/compiler.bash
+++ public/app/plugins/QuizBuilder/compiler.bash
@@ -8,23 +8,20 @@ cd src
 ./compile.bash
 cd ..
 
-exit
-# later we need to add this to minify for publishing to the app folder
-
 echo "Minifying QuizBuilder"
-rm app.min*.css
-rm app.min*.js
-uglifycss app.css --output app.min.$TS.css
+rm -f app.min*.css
+rm -f app.min*.js
+uglifycss edit.css --output app.min.$TS.css
 # uglifyjs --keep-fnames localforage.min.js handlebars.min.js filedrop.js jscolor.min.js --output app.deps.min.js
-uglifyjs app.js --output app.min.$TS.js
+uglifyjs edit.js --output app.min.$TS.js
 
 echo "Updating references"
 
 if [ "$(uname)" == "Darwin" ]; then
-	sed -E -i '' "s/app.css/app.min.$TS.css/g" edit.html
-	sed -E -i '' "s/app.js/app.min.$TS.js/g" edit.html
+	sed -E -i '' "s/edit.css/app.min.$TS.css/g" edit.html
+	sed -E -i '' "s/edit.js/app.min.$TS.js/g" edit.html
 else
 #elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	sed -E --in-place='' "s/app.css/app.min.$TS.css/g" edit.html
-	sed -E --in-place='' "s/app.js/app.min.$TS.js/g" edit.html
+	sed -E --in-place='' "s/edit.css/app.min.$TS.css/g" edit.html
+	sed -E --in-place='' "s/edit.js/app.min.$TS.js/g" edit.html
 fi
