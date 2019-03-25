ad178e517a11f10314fe83a6e5ad9057eeae72f9
Tim St.Clair <tim.stclair@gmail.com>
2019-03-26 09:21:04 +1100

    the /public/app folder should be ignored in git since it's compiled
    deploying should be a case of grabbing the repo and then running the compile_scripts.bash file, which publishes the correct updated version of the /public/app folder.

--- public/app/load.php
+++ public/app/load.php
@@ -1,9 +1,8 @@
 <?php
 require_once('../../vendor/autoload.php');
-$verifier = (new CoursesuiteValidator())->Validate($_GET);
+$verifier = (new CoursesuiteValidator(false,false))->Validate($_GET);
 $verifier->code->minified = true;
-$timestamp = '20190325145156';
-$minified_css = 'css/app.min.20190325145156.css';
-$minified_app = 'js/app.min.20190325145156.js';
-$minified_head = 'js/head.min.20190325145156.js';
+$timestamp = '20190326000113';
+$minified_css = 'css/app.min.20190326000113.css';
+$minified_app = 'js/app.min.20190326000113.js';
 ?>
