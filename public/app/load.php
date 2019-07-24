76a750bac289388456a1b8d5b6514798ffa0cec3
Tim St.Clair <tim.stclair@gmail.com>
2019-07-24 10:44:36 +1000

    new publish

--- public/app/load.php
+++ public/app/load.php
@@ -2,7 +2,7 @@
 require_once('../../vendor/autoload.php');
 $verifier = (new CoursesuiteValidator())->Validate($_GET);
 $verifier->code->minified = true;
-$timestamp = '20190710213049';
-$minified_css = 'css/app.min.20190710213049.css';
-$minified_app = 'js/app.min.20190710213049.js';
+$timestamp = '20190724104421';
+$minified_css = 'css/app.min.20190724104421.css';
+$minified_app = 'js/app.min.20190724104421.js';
 ?>
