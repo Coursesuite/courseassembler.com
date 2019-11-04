80ef3a1aa9d0045c885d642b5c6f5bc4ea58a624
Tim St.Clair <tim.stclair@gmail.com>
2019-11-04 12:35:38 +1100

    Moved headings around, added new demonstration video

--- public/routes/home.inc.php
+++ public/routes/home.inc.php
@@ -1,38 +1,40 @@
-		<div class="uk-section uk-background-cover" data-src="/assets/home_background.svg" uk-img>
+		<div class="uk-section uk-background-cover" data-src="/assets/home_background.svg" uk-img data-width="280" data-height="62">
 			<div class="uk-container">
 				<div class="uk-child-width-1-2@m uk-light" uk-grid>
 					<div>
 						<h1>Use <span id="typed" style="font-weight:600">word documents</span><br>to assemble learning content</h1>
 						<p class="uk-text-lead">It's like an assembly line for your content.</p>
-						<p class="uk-margin-top-large"><a href="#more" class="uk-button uk-button-text">Find out more <span uk-icon="triangle-down"></span></a></p>
+						<iframe class="uk-margin-top uk-box-shadow-large uk-margin-auto@s" src="https://player.vimeo.com/video/321391199" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen uk-responsive uk-video="automute: false; autoplay: false"></iframe>
 					</div>
 					<div>
-						<img src="/assets/hero.svg">
+						<img data-src="assets/hero.svg" data-width="580" data-height="425" uk-img>
 					</div>
 				</div>
 			</div>
 		</div>
 
 		<div class="uk-container uk-margin-large" id="more">
-			<h2 class="uk-hidden@m">Need learning content fast?</h2>
+			<h2 class="uk-text-center">The aim of this tool is to get your content e-learning ready<br><i>in a matter of minutes</i>.</h2>
+
 			<div class="uk-flex-middle" uk-grid>
 				<div class="uk-width-1-3@m uk-flex-last@m">
 					<img data-src="assets/mobile_complication.png" class="uk-hidden@s" uk-img>
-					<img data-src="assets/complication.png" class="uk-visible@s" uk-img>
+					<img data-src="assets/complication.png" class="uk-visible@s" uk-img data-width="330" data-height="330">
 				</div>
 				<div class="uk-width-2-3@m">
+					<h3>Need learning content fast?</h3>
 					<p>Your boss just emailed you a 75 page Word document and said "Make this into an e-learning compliant package".</p>
 					<p>Under that file there's some notes about quiz questions you might add in and that learners need to watch at least half of the company training video.<br><br>The email finishes off <em>"and I need it done yesterday"</em>.</p>
 					<p>Oh boy. This sounds <i>complicated</i>.</p>
 				</div>
 			</div>
-			<h2 class="uk-hidden@m">Robots to the rescue!</h2>
 			<div class="uk-flex-middle uk-child-width-1-2@m" uk-grid>
 				<div class="uk-width-1-3@m">
 					<img data-src="assets/mobile_simplicity.png" class="uk-hidden@s" uk-img>
-					<img data-src="assets/simplicity.png" class="uk-visible@s" uk-img>
+					<img data-src="assets/simplicity.png" class="uk-visible@s" uk-img data-width="330" data-height="330">
 				</div>
 				<div class="uk-width-2-3@m">
+					<h3>Robots to the rescue!</h3>
 					<p><i>Pfft.</i> You've got this.</p>
 					<p><strong>Drag on all your documents.&nbsp;</strong>Add a Quiz using the wizard. Paste in a Vimeo link and drag the score requirement to 50%. Reorder pages to be more logical and remove those blank pages you didn't need. Pick a snazzy design, and press Export.</p>
 					<p>That's it. <strong>You're done.</strong> Hmm, don't let your boss know you're <em>that</em> efficient, they might exect you to be even faster next time!</p>
@@ -40,11 +42,10 @@
 			</div>
 		</div>
 
-		<div class="robot-wave">
+		<div class="robot-wave uk-section">
 		<div class="uk-container uk-margin-xlarge uk-padding-large uk-light uk-text-center">
-			<h2>The aim of this tool is to get your content e-learning ready<br><i>in a matter of minutes</i>.</h2>
 			<div>
-				<iframe class="uk-box-shadow-medium" src="https://player.vimeo.com/video/321391199" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen uk-responsive uk-video="automute: false; autoplay: false"></iframe>
+				<iframe class="uk-box-shadow-medium" src="https://player.vimeo.com/video/370752968" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen uk-responsive uk-video="automute: false; autoplay: false"></iframe>
 			</div>
 		</div>
 		</div>
@@ -56,7 +57,7 @@
 				<div>
 					<div class="uk-card">
 						<div class="uk-card-media-top">
-							<img src="assets/elearning.svg">
+							<img data-src="assets/elearning.svg" data-width="270" data-height="270" uk-img>
 						</div>
 						<div class="uk-card-body">
 							SCORM compliance - works where you need it to. Or you can turn SCORM off and use it like a regular HTML5-compliant web page.
@@ -66,7 +67,7 @@
 				<div>
 					<div class="uk-card">
 						<div class="uk-card-media-top">
-							<img src="assets/responsive.svg">
+							<img data-src="assets/responsive.svg" data-width="270" data-height="270" uk-img>
 						</div>
 						<div class="uk-card-body">
 							Responsive content adapts to fit the screen - it works wherever your learners are, with whatever they have.
@@ -76,7 +77,7 @@
 				<div>
 					<div class="uk-card">
 						<div class="uk-card-media-top">
-							<img src="assets/pencils.svg">
+							<img data-src="assets/pencils.svg" data-width="270" data-height="270" uk-img>
 						</div>
 						<div class="uk-card-body">
 							No-nonsense learner friendly designs that require no design, coding or technical skills. Don't get lost in options - just pick and go.
@@ -86,7 +87,7 @@
 				<div>
 					<div class="uk-card">
 						<div class="uk-card-media-top">
-							<img src="assets/formats.svg">
+							<img data-src="assets/formats.svg" data-width="270" data-height="270" uk-img>
 						</div>
 						<div class="uk-card-body">
 							Works with all your favorite formats, inluding many online sources. We'll convert it to be web friendly. Use the formats you are comfortable with.
@@ -132,23 +133,19 @@
 					</div>
 				</div>
 				<div class="uk-visible@m">
-					<img src="assets/production.png" uk-img>
+					<img data-src="assets/production.png" data-width="580" data-height="428" uk-img>
 				</div>
 			</div>
 		</div>
 
 		<div class="uk-section uk-background-muted">
 			<div class="uk-container uk-margin-large uk-padding-large uk-text-center">
-				<hr class="uk-divider-icon">
 
-				<h3>For the price of a coffee and a <abbr title="- or 🍩 or 🍪, go with what makes you 😀 ...">bagel</abbr>, put your content out there already!</h3>
-
+				<h3>No recurring fees. No conversion limits or premium content hidden behind paywalls.</h3>
+				<p class="uk-width-2-3@m uk-width-1-1@s uk-margin-auto">Don't overspend on services that limit how much you can convert per month, or require you to subscribe to unlock a full experience, or require you to pay for more time than you really need. You might only need Course Assembler a few days every few months - so buy it when you need it, and don't pay for what you don't need.</p>
 				<nav uk-navbar class="my-launch-bottom"><div class="uk-navbar-center"><?php include "_launch.inc.php"; ?></div></nav>
 
-
-
 				<p class="uk-hidden@m uk-text-emphasis my-peek uk-box-shadow-medium uk-border-rounded">Web-based app requires a modern Desktop browser</p>
-				<hr class="uk-divider-icon">
 			</div>
 		</div>
 
