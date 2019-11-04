30562a7e0bcd78f0bd0faf8b664eb39a67018394
Tim St.Clair <tim.stclair@gmail.com>
2019-11-04 12:40:33 +1100

    republished

--- public/app/load.php
+++ public/app/load.php
@@ -4,7 +4,7 @@ session_start();
 if (!isset($_SESSION['sesskey'])) $_SESSION['sesskey'] = md5(time());
 $verifier = (new CoursesuiteValidator())->Validate($_GET);
 $verifier->code->minified = true;
-$timestamp = '20191028145326';
-$minified_css = 'css/app.min.20191028145326.css';
-$minified_app = 'js/app.min.20191028145326.js';
+$timestamp = '20191104124011';
+$minified_css = 'css/app.min.20191104124011.css';
+$minified_app = 'js/app.min.20191104124011.js';
 ?>
