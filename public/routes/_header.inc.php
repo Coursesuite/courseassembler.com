c5285a7074aa8671b50c3339d3ac2637b19b60a0
Tim St.Clair <tim.stclair@gmail.com>
2019-11-04 12:34:53 +1100

    updated metadata for better indexing

--- public/routes/_header.inc.php
+++ public/routes/_header.inc.php
@@ -4,23 +4,23 @@
 		<meta charset="utf-8"/>
 		<meta content="IE=edge" http-equiv="X-UA-Compatible"/>
 		<meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport"/>
-		<meta name="description" content="Rapidly convert your learning content to HTML5, add quizzes and video and package with a SCORM wrapper, in a matter of minutes" />
-		<meta name="keywords" content="Course Assembler, scorm modules, scorm content, scorm wrapper, scorm authoring tool, scorm packages ppt to scorm, pptx to scorm, powerpoint to scorm, docx to scorm, pdf to scorm, video to scorm, google slides to scorm, google docs to scorm" />
+		<meta name="description" content="Rapidly convert your learning content to HTML5, add quizzes and video and package with a SCORM wrapper, all in a matter of minutes using our web-based tool." />
+		<meta name="keywords" content="pdf to scorm, docx to scorm, ppt to scorm, mp4 to scorm, powerpoint to scorm, scorm elearning, scorm authoring, scorm converter, coursesuite, scormcloud authoring, moodle scorm, scorm modules, scorm content, scorm wrapper, scorm tool, scorm packager, ppt to scorm, docx to scorm, convert pdf to html, video to scorm, google slides to scorm, google docs to scorm, scorm quiz, quiz to scorm, blackboard, moodle, d2l, rustici, scorm api, scorm2004" />
 		<meta name="author" content="www.coursesuite.com" />
-		<title><?php echo $page_title; ?> - Course Assembler</title>
+		<title>Course Assembler by Coursesuite - get your content e-learning SCORM ready in a matter of minutes.</title>
 		<meta property="og:locale" content="en_AU">
 		<meta property="og:type" content="website">
-		<meta property="og:title" content="<?php echo $page_title; ?> - Course Assembler">
+		<meta property="og:title" content="<?php echo $page_title; ?> - Course Assembler by Coursesuite">
 		<meta property="og:description" content="Make your content e-learning ready, add quizzes and video and package with a SCORM wrapper, in a matter of minutes.">
 		<meta property="og:url" content="https://<?php echo $_SERVER['SERVER_NAME']; ?>/">
-		<meta property="og:site_name" content="Course Assembler">
+		<meta property="og:site_name" content="Course Assembler ">
 		<meta property="og:image" content="https://<?php echo $_SERVER['SERVER_NAME']; ?>/assets/meta_card.png">
 		<link href="assets/style.css" rel="stylesheet" type="text/css"/>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css" />
         <script src="https://polyfill.io/v3/polyfill.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit-icons.min.js" defer></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js"></script>
 		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 		<?php include "_cookie.inc.php"; ?>
