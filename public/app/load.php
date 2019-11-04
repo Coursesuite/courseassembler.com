fd5466bfb9eb79baea852c65af7d560004fef337
Tim St.Clair <tim.stclair@gmail.com>
2019-11-04 23:11:28 +1100

    recompile everything, again

--- public/app/load.php
+++ public/app/load.php
@@ -4,7 +4,7 @@ session_start();
 if (!isset($_SESSION['sesskey'])) $_SESSION['sesskey'] = md5(time());
 $verifier = (new CoursesuiteValidator())->Validate($_GET);
 $verifier->code->minified = true;
-$timestamp = '20191104124011';
-$minified_css = 'css/app.min.20191104124011.css';
-$minified_app = 'js/app.min.20191104124011.js';
+$timestamp = '20191104231106';
+$minified_css = 'css/app.min.20191104231106.css';
+$minified_app = 'js/app.min.20191104231106.js';
 ?>
