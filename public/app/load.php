f956417c14db47d72fdf346da4c04ad4f34f4cf4
Tim St.Clair <tim.stclair@gmail.com>
2020-04-24 19:38:11 +1000

    added print button; added nicer icons to pdf toolbar

--- public/app/load.php
+++ public/app/load.php
@@ -4,7 +4,7 @@ session_start();
 if (!isset($_SESSION['sesskey'])) $_SESSION['sesskey'] = md5(time());
 $verifier = (new CoursesuiteValidator())->Validate($_GET);
 $verifier->code->minified = true;
-$timestamp = '20200420005533';
-$minified_css = 'css/app.min.20200420005533.css';
-$minified_app = 'js/app.min.20200420005533.js';
+$timestamp = '20200424192912';
+$minified_css = 'css/app.min.20200424192912.css';
+$minified_app = 'js/app.min.20200424192912.js';
 ?>
