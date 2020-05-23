7e1442bb890b0ec23224a4dc3aa95e02fa3541d9
Tim St.Clair <tim.stclair@gmail.com>
2020-05-23 18:55:31 +1000

    Noted H5P on homepage

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,7 +1,7 @@
 function loadTyped() {
 	if (document.querySelector("#typed"))
 		new Typed("#typed",{
-			strings: ["Word documents","any PDF file","a KeyNote file","Google Docs","PowerPoint","Google Slides","Pages","Vimeo videos","YouTube videos","SlideShare","any document"],
+			strings: ["Word documents","any PDF file","a KeyNote file","a H5P package", "Google Docs", "Markdown", "PowerPoint","Google Slides","Pages","Vimeo videos","YouTube videos","SlideShare","any document"],
 			typeSpeed: 90,
 			backSpeed: 45,
 			startDelay: 800,
