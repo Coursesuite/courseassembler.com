6fe368d60f9799cc7b836d23501e00ce6d350681
TIm St.Clair <tim.stclair@gmail.com>
2021-03-30 16:53:31 +1100

    Added Intro plugin

---
+++ src/plugins/Intro/edit.js
@@ -0,0 +1,103 @@
+localforage.config({name: 'DocumentNinja'});
+
+var fileid = window.location.search.split("?")[1] || "";
+
+var payload = {
+	template: 1,
+	url: "https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80",
+	image: "",
+	color1: "#FBAB7E",
+	color2: "#F7CE68",
+	backgroundColour: "#f8f8f8",
+	html: undefined
+}
+
+function save() {
+	var iframe = document.querySelector("iframe[id='frame-" + payload.template + "']");
+	updateFrame(iframe, true);
+}
+
+function updateFrame(iframe, save) {
+	var index = iframe.id.replace("frame-","");
+	fetch("./style" + index + ".html", {cache:"no-store"})
+		.then(function(response) {
+			return response.text()
+		})
+		.then(function(html) {
+			return new Promise(function(resolve) {
+				resolve(parent.DocNinja.Plugins.Intro.Compile(html, payload));
+			});
+		})
+		.then(function(html) {
+			payload.html = html;
+			var blob = new Blob([html],{type:"text/html"});
+			var url = URL.createObjectURL(blob);
+			iframe.src = url;
+			return;
+		})
+		.then(function() {
+			return localforage.getItem(fileid);
+		})
+		.then(function(obj) {
+			obj.payload = payload;
+			return obj;
+		})
+		.then(function(obj) {
+			if (save) localforage.setItem(fileid, obj);
+		});
+}
+
+// load the content of each iframe and replace various payload properties in the source and update the iframes
+function updateFrames(save) {
+	[].forEach.call(document.querySelectorAll("iframe[id]"), function(iframe) {
+		updateFrame(iframe, save);
+	});
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+
+	localforage.getItem(fileid).then(function(obj) {
+
+		Object.assign(payload, obj.payload);
+
+		document.querySelector(".slider a[href='#frame-" + payload.template + "']").click();
+		document.querySelector("#bg").value = payload.backgroundColour;
+		document.querySelector("#color1").value = payload.color1;
+		document.querySelector("#color2").value = payload.color2;
+		document.querySelector("#url").value = payload.url || "";
+
+		updateFrames(false);
+
+	});
+
+	document.querySelector("#url").addEventListener('blur', function(el) { payload.url = this.value; payload.image = undefined; updateFrames(true); });
+	document.querySelector("#bg").addEventListener('change', function(el) { payload.backgroundColour = this.value; updateFrames(true); });
+	document.querySelector("#color1").addEventListener('change', function(el) { payload.color1 = this.value; updateFrames(true); });
+	document.querySelector("#color2").addEventListener('change', function(el) { payload.color2 = this.value; updateFrames(true); });
+
+	document.querySelector("#file").addEventListener('change', function(fileEvent) {
+		if (fileEvent.target && fileEvent.target.files[0].type.indexOf("image/")===-1) return;
+		var reader = new FileReader();
+		reader.onloadend = function (event) {
+			payload.image = event.target.result;
+			payload.url = undefined;
+			document.querySelector("#url").value = "";
+			updateFrames(true);
+		}
+		reader.readAsDataURL(fileEvent.target.files[0]);
+	});
+
+	document.querySelector("main").addEventListener('click', function(e) {
+		if (e.target.dataset.action) {
+			[].forEach.call(document.querySelectorAll('.slider a[data-action]'), function (elm) {
+				var action = (elm === e.target) ? "add" : "remove";
+				if (action === "add") {
+					payload.template = ~~elm.textContent;
+					if (e.x > 0) save();
+				}
+				elm.classList[action]("active");
+			});
+		}
+	})
+
+});
---
+++ src/plugins/Intro/plugin.js
@@ -0,0 +1,55 @@
+(function (DocNinja, undefined) {
+
+	DocNinja.Plugins = DocNinja.Plugins || {};
+	DocNinja.Plugins.Intro = DocNinja.Plugins.Intro || {};
+
+	DocNinja.Plugins.Intro.RegisterPlugin = function(context) {
+		DocNinja.routines.RegisterAction({
+			plugin: context,
+			icon: '<div class="ga-item"><a data-action="add-intro-page"><i class="ninja-layout"></i>Intro Page</a></div>',
+			type: 'page',
+			order: 4,
+			supports: ['edit','view','compile'],
+			handler: 'add-intro-page'
+		});
+	};
+
+	DocNinja.Plugins.Intro.Add = function () {
+		var newId = DocNinja.PurityControl.Nav.GetFileId(),
+			fileInfo = {
+				name: "Course Introduction",
+				kind:"plugin",
+				plugin: "Intro",
+				depth: 0,
+				payload: {"html": Handlebars.templates["intro-page"]()}
+			};
+		localforage.setItem(newId, fileInfo).then(function(obj) {
+			DocNinja.PurityControl.Nav.Add(DocNinja.navItems, newId, fileInfo, null, "ready");
+			DocNinja.PurityControl.Nav.Check();
+			DocNinja.filePreview.Select(newId);
+			localforage.setItem("order", DocNinja.navItems.innerHTML);
+		});
+	}
+
+	// html: loaded from plugins/Intro/styleX.html
+	// payload: obj.payload object from localstorage instance
+	DocNinja.Plugins.Intro.Compile = function (html, payload, strings) {
+		if (!html) return '';
+		var doc = document.implementation.createHTMLDocument('untitled');
+		doc.documentElement.innerHTML = html;
+		var src = payload.image || payload.url;
+		if (src.length && doc.querySelector("img[src]")) doc.querySelector("img[src]").src = src;
+		var css = doc.querySelector("style").textContent;
+		css = css.replace("background-color: #f8f8f8;", "background-color: " + payload.backgroundColour + ";");
+		css = css.replace("background-color: #FBAB7E;", "background-color: " + payload.color1 + ";");
+		css = css.replace("background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);", "background-image: linear-gradient(62deg, "+payload.color1+" 0%, "+payload.color2+" 100%);");
+		doc.querySelector("style").textContent = css;
+		if (strings) {
+			doc.querySelector(".name").textContent = strings['option-course-name'];
+			doc.querySelector(".description").textContent = strings['option-course-description'];
+			doc.querySelector(".copyright").textContent = strings['option-course-copyright'];
+		}
+		return "<!DOCTYPE html>" + doc.documentElement.outerHTML;
+	}
+
+})(window.DocNinja = window.DocNinja || {});
