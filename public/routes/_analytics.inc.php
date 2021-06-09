c244fa2c3311d7b282b2e54f2ba2ba1e6b227a52
TIm St.Clair <tim.stclair@gmail.com>
2021-06-09 22:33:09 +1000

    updated analytics id

--- public/routes/_analytics.inc.php
+++ public/routes/_analytics.inc.php
@@ -1,9 +1,10 @@
 <?php defined("APP")?assert(true):die(); ?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-68767047-6"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MHM2Q0707G"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-68767047-6');
+
+  gtag('config', 'G-MHM2Q0707G');
 </script>
