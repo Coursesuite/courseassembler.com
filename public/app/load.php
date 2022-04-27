5c34f95ad7fc80b44e36fc377fb5b9229c958e7d
TIm St.Clair <tim.stclair@gmail.com>
2022-04-27 20:36:32 +1000

    recompiled app

--- public/app/load.php
+++ public/app/load.php
@@ -3,7 +3,7 @@ require_once('../../vendor/autoload.php');
 session_start();
 if (!isset($_SESSION['sesskey'])) $_SESSION['sesskey'] = md5(time());
 $verifier = Licence::validate(Request::get('hash'));
-$timestamp = '20211130161233';
-$minified_css = 'css/app.min.20211130161233.css';
-$minified_app = 'js/app.min.20211130161233.js';
+$timestamp = '20220427203343';
+$minified_css = 'css/app.min.20220427203343.css';
+$minified_app = 'js/app.min.20220427203343.js';
 ?>
