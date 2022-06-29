1052d0f65cd6793b01a7424d5e42f9c70618a19b
TIm St.Clair <tim.stclair@gmail.com>
2022-06-29 13:38:15 +1000

    republished

--- public/app/load.php
+++ public/app/load.php
@@ -3,7 +3,7 @@ require_once('../../vendor/autoload.php');
 session_start();
 if (!isset($_SESSION['sesskey'])) $_SESSION['sesskey'] = md5(time());
 $verifier = Licence::validate(Request::get('hash'));
-$timestamp = '20220629103813';
-$minified_css = 'css/app.min.20220629103813.css';
-$minified_app = 'js/app.min.20220629103813.js';
+$timestamp = '20220629133324';
+$minified_css = 'css/app.min.20220629133324.css';
+$minified_app = 'js/app.min.20220629133324.js';
 ?>
