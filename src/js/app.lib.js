18b13efb87200f96e37848b080a30c608e7541d5
TIm St.Clair <tim.stclair@gmail.com>
2022-10-20 12:35:00 +1100

    emit handler

--- src/js/app.lib.js
+++ src/js/app.lib.js
@@ -2075,3 +2075,23 @@ function getCSSVariable(name) {
 function setCSSVariable(name, value) {
 	DocNinja.options.root.style.setProperty(name, value);
 }
+
+/**
+ * Emit a custom event
+ * @param  {String} type   The event type
+ * @param  {*}      detail Any details to pass along with the event
+ * @param  {Node}   elem   The element to emit the event on
+ */
+function emit (type, detail, elem = document) {
+
+	// Create a new event
+	let event = new CustomEvent(type, {
+		bubbles: true,
+		cancelable: true,
+		detail: detail
+	});
+
+	// Dispatch the event
+	return elem.dispatchEvent(event);
+
+}
