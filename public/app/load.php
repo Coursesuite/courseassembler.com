383a6616e56bb82c80d2f8aaaba43d249cfcdd13
TIm St.Clair <tim.stclair@gmail.com>
2022-12-25 11:52:32 +1100

    version bump again

--- public/app/load.php
+++ public/app/load.php
@@ -3,7 +3,7 @@ require_once('../../vendor/autoload.php');
 session_start();
 if (!isset($_SESSION['sesskey'])) $_SESSION['sesskey'] = md5(time());
 $verifier = Licence::validate(Request::get('hash'));
-$timestamp = '20221225114447';
-$minified_css = 'css/app.min.20221225114447.css';
-$minified_app = 'js/app.min.20221225114447.js';
+$timestamp = '20221225115222';
+$minified_css = 'css/app.min.20221225115222.css';
+$minified_app = 'js/app.min.20221225115222.js';
 ?>
