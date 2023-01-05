03d2d490f3382c11d6e8cdf271f80308e06d07e6
TIm St.Clair <tim.stclair@gmail.com>
2023-01-05 20:03:09 +1100

    cursor playback for video/audio

---
+++ src/js/media.preview.js
@@ -0,0 +1,74 @@
+/**
+ * Create a video or audio preview including the Cursor recording
+ * Video is controlled by the same player control as the audio track
+ * @param DocNinja for access to options
+ */
+export function Preview(DocNinja) {
+
+    const id = DocNinja.filePreview.CurrentFile();
+    const control = document.getElementById('pageMediaPlayer');
+    let mus, cursor, mp4;
+
+    function createVideo() {
+        let video = document.getElementById('video-overlay');
+        if (!video) {
+            video = document.createElement('video');
+            const position = control.getBoundingClientRect();
+            video.src = mp4;
+            video.id = 'video-overlay';
+
+            video.style.width = position.width + 'px';
+            video.style.left = position.left + 'px';
+
+            document.getElementById('add-documents').appendChild(video);
+        }
+        return video;
+    }
+
+    function onpause(event) {
+        const video = document.getElementById('video-overlay');
+        video.pause();
+        if (cursor) {
+            mus.pause();
+        }
+    }
+
+    function onplay(event) {
+        const video = createVideo();
+        video.play();
+        if (cursor) {
+            var ct = event.target.currentTime;
+            mus.cue(ct);
+        }
+    }
+
+    function onended(event) {
+        const video = document.getElementById('video-overlay');
+        if (video) video.parentNode.removeChild(video);
+    }
+
+    localforage.getItem(id).then(function(obj) {
+
+        cursor = property_exists(obj, "payload.cursor");
+
+        if (property_exists(obj, "payload.mp4")) {
+            mp4 = URL.createObjectURL(get_property(obj, "payload.mp4"));
+            createVideo(mp4);
+        }
+
+        if (cursor) {
+            mus = new Mus({
+                target: document.getElementById('preview-frame').contentWindow
+            });
+            mus.setData(get_property(obj, "payload.cursor"));
+        } else if (mus) {
+            mus.release();
+        }
+
+        control.addEventListener('pause', onpause);
+        control.addEventListener('play', onplay);
+        control.addEventListener('ended', onended);
+
+    });
+
+}
